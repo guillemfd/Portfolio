c3019d7fc44c4eb3d555e26f1cd59fea04d5f8e4
Guillem Ferrer <guillemfd@gmail.com>
2022-02-01 16:21:05 +0100

    about sections in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './components/about/about';
 import Intro from './components/intro/intro';
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <About />
     </div>
   );
 }
