963a894fd5aee76457f2a74e1a203112aaeb537e
Guillem Ferrer <guillemfd@gmail.com>
2022-02-02 01:22:48 +0100

    background floating colorful triangle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/about/about';
+import Background from './components/background/background';
 import Intro from './components/intro/intro';
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Intro />
       <About />
     </div>
