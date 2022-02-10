97c87507bfd0ceaadcb936a9a022ee00aa89fa30
Guillem Ferrer <guillemfd@gmail.com>
2022-02-10 16:11:50 +0100

    gallery pending to be responsive

--- src/pages/galleries/galleryOne/galleryOne.jsx
+++ src/pages/galleries/galleryOne/galleryOne.jsx
@@ -1,5 +1,31 @@
 import './galleryOne.css'
+import { useState } from 'react'
+import close from '../../../img/whiteCross.png'
 import img1 from '../../../img/GalleryOne/MiniGeensen-1.jpg'
+import img2 from '../../../img/GalleryOne/MiniGeensen-2.jpg'
+import img3 from '../../../img/GalleryOne/MiniGeensen-3.jpg'
+import img4 from '../../../img/GalleryOne/MiniGeensen-4.jpg'
+import img5 from '../../../img/GalleryOne/MiniGeensen-5.jpg'
+import img6 from '../../../img/GalleryOne/MiniGeensen-6.jpg'
+import img7 from '../../../img/GalleryOne/MiniGeensen-7.jpg'
+import img8 from '../../../img/GalleryOne/MiniGeensen-8.jpg'
+import img9 from '../../../img/GalleryOne/MiniGeensen-9.jpg'
+import img10 from '../../../img/GalleryOne/MiniGeensen-10.jpg'
+import img11 from '../../../img/GalleryOne/MiniGeensen-11.jpg'
+import img12 from '../../../img/GalleryOne/MiniGeensen-12.jpg'
+import img13 from '../../../img/GalleryOne/MiniGeensen-13.jpg'
+import img14 from '../../../img/GalleryOne/MiniGeensen-14.jpg'
+import img15 from '../../../img/GalleryOne/MiniGeensen-15.jpg'
+import img16 from '../../../img/GalleryOne/MiniGeensen-16.jpg'
+import img17 from '../../../img/GalleryOne/MiniGeensen-17.jpg'
+import img18 from '../../../img/GalleryOne/MiniGeensen-18.jpg'
+import img19 from '../../../img/GalleryOne/MiniGeensen-19.jpg'
+import img20 from '../../../img/GalleryOne/MiniGeensen-20.jpg'
+import img21 from '../../../img/GalleryOne/MiniGeensen-21.jpg'
+import img22 from '../../../img/GalleryOne/MiniGeensen-22.jpg'
+import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils'
+
+
 
 const GalleryOne = () => {
 
@@ -7,30 +33,125 @@ const GalleryOne = () => {
     {
       id: 1,
       imgSrc: img1,
-    }
+    },
+    {
+      id: 2,
+      imgSrc: img2,
+    },    
+    {
+      id: 3,
+      imgSrc: img3,
+    },    
+    {
+      id: 4,
+      imgSrc: img4,
+    },   
+    {
+      id: 5,
+      imgSrc: img5,
+    },    
+    {
+      id: 6,
+      imgSrc: img6,
+    },    
+    {
+      id: 7,
+      imgSrc: img7,
+    },
+    {
+      id: 8,
+      imgSrc: img8,
+    },
+    {
+      id: 9,
+      imgSrc: img9,
+    },
+    {
+      id: 10,
+      imgSrc: img10,
+    },
+    {
+      id: 11,
+      imgSrc: img11,
+    },
+    {
+      id: 12,
+      imgSrc: img12,
+    },    
+    {
+      id: 13,
+      imgSrc: img13,
+    },    
+    {
+      id: 14,
+      imgSrc: img14,
+    },   
+    {
+      id: 15,
+      imgSrc: img15,
+    },    
+    {
+      id: 16,
+      imgSrc: img16,
+    },    
+    {
+      id: 17,
+      imgSrc: img17,
+    },
+    {
+      id: 18,
+      imgSrc: img18,
+    },
+    {
+      id: 19,
+      imgSrc: img19,
+    },
+    {
+      id: 20,
+      imgSrc: img20,
+    },
+    {
+      id: 21,
+      imgSrc: img21,
+    },
+    {
+      id: 22,
+      imgSrc: img22,
+    },
   ]
 
 
+  const [modal, setModal] =useState(false)
+  const [tempImgSrc, setTempImgSrc] = useState('')
+
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc)
+    setModal(true)
+  }
+
+
   return (
-    <div>
+  <>
         <h1 className="g-title">Architect's office</h1>
         <p className="g-desc">
             Shooting session to a small and cozy architect's office. Wooden finishes, hidden illumination.
         </p>
 
+        <div className={modal? "modal open" : "modal"}>
+          <img src={tempImgSrc} alt="modal" />
+          <img src={close} alt="closeCross" className='closeCross' onClick={() => setModal(false)}/>
+        </div>
+
         <div className="gallery">
           {data.map((item, index) =>{
-            return(
-                <div className="pics" key={index}>
-                    <img src={item.imgSrc} alt={item.id} />
-                </div>
-            )
-        })}
-</div>
-
-
-
-    </div>        
+              return(
+                  <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
+                      <img src={item.imgSrc} alt={item.id} className="pics-img"/>
+                  </div>
+              )
+          })}
+        </div>
+    </>        
   );
 };
 
